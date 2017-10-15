017fa94955bc370f406124f2256da996272cb512
Chad Furman <chad@chadfurman.com>
2017-10-14 21:57:42 -0400

    hearme bound

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import fetch from 'whatwg-fetch'
 
+document.getElementById('begin-button').addEventListener('click', hearMe);
+
 function hearMe() {
   fetch('/index.html').then(response => {
     console.log(response)
   })
 }
+
+
